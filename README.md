43190e94d491af4a1e6f125ecbdd14b20980d260
Marília Druzian <mariliadruzian@gmail.com>
2022-12-05 14:48:43 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Sobre
+**Monitor de Sites** é o meu primeiro projeto utilizandoo a linguagem **Golang**. Foi criado durante o curso [Go: a linguagem do Google](https://cursos.alura.com.br/course/golang), ministrado pelo professor [Douglas Quintanilha](https://www.linkedin.com/in/douglas-quintanilha). O objetivo desse projeto foi me introduzir na linguagem Go.
+
+## Funcionalidade
+A funcionalidade do projeto é monitorar sites pré-determinados em um arquivo **.txt**. O script exibe o status code dos endereços, gera um arquivo contendo os logs e exibe um relatório com as informações registradas.
+
+## Tecnologias utilizadas
+- Golang
+- Git
+
+##  Rodando o projeto
+Tenha instalado em seu computador o [Git](https://git-scm.com) e o [Go](https://go.dev).
+
+```bash
+# Clone this project
+$ git clone https://github.com/mariliadruzian/monitor-sites
+
+# Access
+$ cd monitor-sites
+
+# Run the project
+$ go run monitor.go
+```
+
+## Licença
+Esse projeto está sob licença MIT. Verifique o arquivo **[LICENSE](LICENSE.md)** para mais detalhes.
+
+#
+
+Coded by [@mariliadruzian](https://beacons.ai/mariliadruzian) 🌱
---
+++ monitor.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+const monitoramento = 3
+const delay = 5
+
+func main() {
+	exibeIntroducao()
+
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			imprimeLogs()
+		case 3:
+			fmt.Println("\n" + "Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando inválido!")
+			os.Exit(-1)
+		}
+	}
+}
+
+func exibeIntroducao() {
+	versao := 1.2
+	fmt.Println("\n" + "Olá! Bem-vindo(a) ao monitor de sites.")
+	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("")
+}
+
+func exibeMenu() {
+	fmt.Println("---------------------------------------------------------")
+	fmt.Println("\n" + "Escolha uma das opções do Menu:")
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("3 - Sair do programa")
+	fmt.Println("")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi o", comandoLido)
+	fmt.Println("")
+	fmt.Println("---------------------------------------------------------")
+	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("\n" + "Monitorando...")
+	fmt.Println("")
+	sites := leSitesDoArquivo()
+
+	for i := 0; i < monitoramento; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("Monitoramento finalizado com sucesso!")
+	fmt.Println("")
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+		registraLog(site, true)
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status code:", resp.StatusCode)
+		registraLog(site, false)
+	}
+}
+
+func leSitesDoArquivo() []string {
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+			break
+		}
+	}
+
+	arquivo.Close()
+	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocoreu um erro:", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+	fmt.Println("\n" + "Exibindo logs...")
+	fmt.Println("")
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(arquivo))
+}
